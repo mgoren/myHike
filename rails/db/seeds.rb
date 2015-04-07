c9784c42189ddbb25d5b845a5d0f5f0e72e0fc41
Mike <mgoren@mortalwombat.net>
2015-04-07 14:05:15 -0700

    add devise to rails backend

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([
+  {email: 'mike@mike.mike',
+   password: 'password', password_confirmation: 'password' },
+  {email: 'tina@tina.tina',
+   password: 'password', password_confirmation: 'password' },
+  {email: 'jake@jake.jake',
+   password: 'password', password_confirmation: 'password' }
+])
