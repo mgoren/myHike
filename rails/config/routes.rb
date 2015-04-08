bcfd402a690ab5732c504bd56d03c3fb38371cb3
Mike <mgoren@mortalwombat.net>
2015-04-08 14:27:07 -0700

    Route devise registration through Api adapater

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
 
   root to: 'trails#index'
 
-  devise_for :users, controllers: { sessions: 'sessions' }
 
   resources :users, only: [:index, :show] do
     resources :ratings, except: [:new, :edit]
   end
 
+  devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations' }
+
+
   resources :trails, except: [:new, :edit]
 
   namespace :api do
     get :csrf, to: 'csrf#index'
+    devise_scope :user do
+      post "/users" => "registrations#create"
+    end
   end
 
 end
