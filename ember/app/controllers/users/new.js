450823368cb7febbd025c381664acef3a1e64570
Jake Kaad <kaadalac@gmail.com>
2015-04-08 09:39:41 -0700

    add user registration

---
+++ ember/app/controllers/users/new.js
@@ -0,0 +1,13 @@
+/* globals $ */
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  authenticator: 'simple-auth-authenticator:devise',
+  actions: {
+    save: function(){
+      var user = this.get('model');
+      user.save();
+      this.transitionToRoute('login');
+    }
+  }
+});
