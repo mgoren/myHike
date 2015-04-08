bb7d6cbef6b825933eefc8df1277f8e1c6f10ee9
Mike <mgoren@mortalwombat.net>
2015-04-08 10:20:54 -0700

    ember init

--- rails/app/controllers/trails_controller.rb
+++ rails/app/controllers/trails_controller.rb
@@ -2,6 +2,9 @@ class TrailsController < ApplicationController
 
   def index
     @trails = Trail.all
+      respond_to do |format|
+        format.json { render json: @trails }
+      end
   end
 
 end
