b4692f6ded9418060446da196ed2241a83f72e74
Alex Kaufman <alexkaufman06@gmail.com>
2015-04-08 14:37:43 -0700

    Add templates and routes for hikes, maps, and about pages

--- ember/app/router.js
+++ ember/app/router.js
@@ -13,4 +13,16 @@ export default Router.map(function() {
   this.route('users', function() {
     this.route('new');
   });
+
+  this.route('hikes', function() {
+    this.route('index');
+  });
+
+  this.route('maps', function() {
+    this.route('index');
+  });
+
+  this.route('about', function() {
+    this.route('index');
+  });
 });
