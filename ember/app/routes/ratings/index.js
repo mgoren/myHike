004b14390dcc6e3f98644f1895285bc1469a8f18
Mike Goren <mgoren@mortalwombat.net>
2015-04-09 11:36:15 -0700

    merge with front-end vid

--- ember/app/routes/ratings/index.js
+++ ember/app/routes/ratings/index.js
@@ -1,11 +1,7 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
   model: function(params) {
-=======
-  model: function() {
->>>>>>> abc2ee647f2ebdaf5205b55504fa15ca132df3bd
     return this.store.find('rating');
   }
 });
