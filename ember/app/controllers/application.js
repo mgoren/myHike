c15a07705bd113adb6437274ebf3a8b6b4591d69
joey sandine <jasandine@gmail.com>
2015-04-09 14:25:01 -0700

    create searchbox div

--- ember/app/controllers/application.js
+++ ember/app/controllers/application.js
@@ -1,20 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-trails: (function() {
-    if (this.get('search')) {
-      return this.get('searchedTrails');
-    } else {
-      return this;
+  showSearch: false,
+  actions: {
+    setShowSearch: function() {
+      Ember.set(this, 'showSearch', true);
+    },
+    hideSearchBox: function() {
+      Ember.set(this, 'search', '')
+      Ember.set(this, 'showSearch', false);
     }
-  }).property('search', 'searchedTrails'),
-  searchedTrails: (function() {
-    var search;
-    search = this.get('search').toLowerCase();
-    return this.filter((function(_this) {
-      return function(trail) {
-        return trail.get('name').toLowerCase().indexOf(search) !== -1;
-      };
-    })(this));
-  }).property('search', '@each.name')
+  },
+  trails: (function() {
+      if (this.get('search')) {
+        return this.get('searchedTrails');
+      } else {
+        return this;
+      }
+    }).property('search', 'searchedTrails'),
+    searchedTrails: (function() {
+      var search;
+      search = this.get('search').toLowerCase();
+      return this.filter((function(_this) {
+        return function(trail) {
+          return trail.get('name').toLowerCase().indexOf(search) !== -1;
+        };
+      })(this));
+    }).property('search', '@each.name')
 });
