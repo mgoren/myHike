7eed32ab7696f330e81e13d7296047d7a4bbb524
David <zulaica@gmail.com>
2015-04-09 11:17:57 -0700

    Use ember-google-map to display map data from the database.

---
+++ ember/app/controllers/maps/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  zoom: 8,
+  lat: 45.5230622,
+  lng: -122.6764816
+});
---
+++ ember/app/controllers/maps/my-marker.js
@@ -0,0 +1,6 @@
+import GoogleMapMarkerController from '../google-map/marker';
+
+export default GoogleMapMarkerController.extend({
+  lat: Ember.computed.alias('this.latitude'),
+  lng: Ember.computed.alias('this.longitude')
+});
