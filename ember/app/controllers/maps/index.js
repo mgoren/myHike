89bae47c4e44004524ebd1b796cfbcd0241d3f09
David <zulaica@gmail.com>
2015-04-10 10:34:34 -0700

    Remove attempt at geolocation until feature is ready; default view set to Portland.

--- ember/app/controllers/maps/index.js
+++ ember/app/controllers/maps/index.js
@@ -1,38 +1,8 @@
 import Ember from 'ember';
 
-// Can't seem to get Geolocation to work properly. Shelving this for now.
-
-// Get user's coordinates if able
-// function askForLocation() {
-  var lat, lng;
-
-  if (navigator.geolocation) {
-    // Fallback coordinates for when Geolocation isn't available/allowed.
-    lat = 37.7749295;   // San Francisco
-    lng = -122.4194155; // San Francisco
-    lat = 45.5230622;   // Portland
-    lng = -122.6764816; // Portland
-    navigator.geolocation.getCurrentPosition(
-    // Success
-    function(position) {
-      // debugger;
-      lat = position.coords.latitude;
-      lng = position.coords.longitude;
-      // map.setCenter(new google.maps.LatLng(lat, lng));
-    },
-    // Failure
-    function() {
-      // Some code
-    });
-  } else {
-    // Fallback for UA's witout geolocation
-  }
-// }
-
-// google.maps.event.addDomListener(window, 'load', askForLocation);
-
 export default Ember.ArrayController.extend({
   zoom: 8,
-  lat: lat,
-  lng: lng
+  // Default initial map view to Portland
+  lat: 45.5230622,
+  lng: -122.6764816
 });
