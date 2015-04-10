ba3b49dc7e1ec8109753e73acc3dd6dbdef40ae6
David <zulaica@gmail.com>
2015-04-10 13:11:11 -0700

    Attempt to access googleMap object.

--- ember/app/controllers/maps/index.js
+++ ember/app/controllers/maps/index.js
@@ -5,14 +5,15 @@ export default Ember.ArrayController.extend({
     setLocation: function() {
       if (navigator.geolocation) {
         // Fallback coordinates for when Geolocation isn't available/allowed.
-        // Ember.set(this, "lat", 37.7749295); // San Francisco
-        // Ember.set(this, "lng", -122.4194155); // San Francisco
         Ember.set(this, "lat", 45.5230622);
         Ember.set(this, "lng", -122.6764816);
+        Ember.set(this, "lat", 37.7749295); // San Francisco
+        Ember.set(this, "lng", -122.4194155); // San Francisco
         navigator.geolocation.getCurrentPosition(
         // Success
         function(position) {
           console.log("Geolocation Success");
+          console.log("googleMap: " + googleMap);
           Ember.set(this, "lat", position.coords.latitude);
           Ember.set(this, "lng", position.coords.longitude);
         },
