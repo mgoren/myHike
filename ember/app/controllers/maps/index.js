cef93b196db16bdd32d44da3333f41103031b458
David Zulaica <zulaica@gmail.com>
2015-04-11 13:54:20 -0700

    Finish geolocation action.

--- ember/app/controllers/maps/index.js
+++ ember/app/controllers/maps/index.js
@@ -1,33 +1,41 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
+export default Ember.Controller.extend({
   actions: {
-    setLocation: function() {
+    getLocation: function() {
+      var lat, lng;
+      var map = this;
       if (navigator.geolocation) {
-        // Fallback coordinates for when Geolocation isn't available/allowed.
-        Ember.set(this, "lat", 37.7749295); // San Francisco
-        Ember.set(this, "lng", -122.4194155); // San Francisco
-        Ember.set(this, "lat", 45.5230622);
-        Ember.set(this, "lng", -122.6764816);
+        // Fallback coordinates in case something goes horribly wrong.
+        lat = 37.7749295;   // San Francisco
+        lng = -122.4194155; // San Francisco
         navigator.geolocation.getCurrentPosition(
-        // Success
-        function(position) {
-          console.log("Geolocation Success");
-          console.log("googleMap: " + googleMap);
-          Ember.set(this, "lat", position.coords.latitude);
-          Ember.set(this, "lng", position.coords.longitude);
-        },
-        // Failure
-        function() {
-          console.log("Geolocation Failure");
-          Ember.set(this, "lat", 45.5230622);
-          Ember.set(this, "lng", -122.6764816);
-        });
+          // getLocation Success
+          function(position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+            map.setProperties({lat: lat, lng: lng});
+          },
+          // getLocation Failure
+          function() {
+            lat = 34.0522342;   // Los Angeles
+            lng = -118.2436849; // Los Angeles
+            map.setProperties({lat: lat, lng: lng});
+          },
+          // getLocation Settings
+          { maximumAge: 60000,
+            timeout: 7000,
+            enableHighAccuracy: true
+          });
       } else {
-        console.log("Geolocation non-existant");
-        Ember.set(this, "lat", 45.5230622);
-        Ember.set(this, "lng", -122.6764816);
+        // getLocation not available
+        lat = 37.7749295;   // San Francisco
+        lng = -122.4194155; // San Francisco
+        map.setProperties({lat: lat, lng: lng});
       }
+      // Ensure the googleMap object has something to work with when initially
+      // loaded.
+      map.setProperties({lat: lat, lng: lng});
     }
   },
   zoom: 8
